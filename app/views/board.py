22471171a9064bf41ddf721ccb06ed1d30d36fee
2cong <cjina0418@gmail.com>
2020-08-22 13:28:47 +0900

    add: 게시판 조회 api 추가

--- app/views/board.py
+++ app/views/board.py
@@ -1,4 +1,18 @@
 import json
 
+from app.models import Board
 from flask_classful import FlaskView, route
-from flask import request
+from flask import jsonify, request
+
+class BoardView(FlaskView):
+    # 게시판 목록
+    @route('/', methods=['GET'])
+    def list_board(self):
+
+        board_list = Board.objects(is_deleted = True)
+
+        board_data = [
+            {"name": board.name}
+        for board in board_list]
+
+        return jsonify(data=board_data), 200
