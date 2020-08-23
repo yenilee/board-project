bc3178f92d5b065eaca71574037b053c1b3af231
2cong <cjina0418@gmail.com>
2020-08-23 12:42:11 +0900

    Add : 게시판 추가 api 생성

--- app/views/board.py
+++ app/views/board.py
@@ -5,14 +5,29 @@ from flask_classful import FlaskView, route
 from flask import jsonify, request
 
 class BoardView(FlaskView):
-    # 게시판 목록
+    # 게시판 이름 목록
     @route('/', methods=['GET'])
-    def list_board(self):
+    def get_board_menu(self):
 
-        board_list = Board.objects(is_deleted = True)
+        board_data = Board.objects(is_deleted = True)
 
-        board_data = [
+        board_menu = [
             {"name": board.name}
-        for board in board_list]
+        for board in board_data]
 
-        return jsonify(data=board_data), 200
+        return jsonify(data=board_menu), 200
+
+    # 게시판 생성
+    @route('', methods=['POST'])
+    def post(self):
+
+        name = json.loads(request.data)['name']
+
+        # 현재 존재하는 board와 이름 중복 인
+        if Board.objects(name=name, is_deleted=False):
+            return jsonify(message='이미 등록된 게시판입니다.'), 409
+
+        board = Board(name=name)
+        board.save()
+
+        return '', 200
