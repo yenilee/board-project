fea772b37feeb046cdc6e9152f4d1f007d1cb439
2cong <cjina0418@gmail.com>
2020-08-23 20:55:09 +0900

    Add:게시판 목록 조회

--- app/views/board.py
+++ app/views/board.py
@@ -1,39 +1,61 @@
 import json
 
-from app.models     import Board, Post
-from flask_classful import FlaskView, route
-from flask          import jsonify, request, g
-from app.utils      import auth
+from app.models         import Board, Post
+from flask_classful     import FlaskView, route
+from flask              import jsonify, request, g
+from app.utils          import auth
 
 
 class BoardView(FlaskView):
-    # 게시판 이름 목록
-    @route('/', methods=['GET'])
-    def get_board_menu(self):
+    # 게시판 카테고리
+    @route('/category', methods=['GET'])
+    def get_board_category(self):
+        board_data = Board.objects(is_deleted = False)
 
-        board_data = Board.objects(is_deleted = True)
-
-        board_menu = [
+        board_category = [
             {"name": board.name}
         for board in board_data]
 
-        return jsonify(data=board_menu), 200
+        return jsonify(data=board_category), 200
 
 
     # 게시판 생성
     @route('', methods=['POST'])
     def post(self):
-
         name = json.loads(request.data)['name']
 
-        # 현재 존재하는 board와 이름 중복 인
+        # 현재 존재하는 board와 이름 중복 확인
         if Board.objects(name=name, is_deleted=False):
-            return jsonify(message='이미 등록된 게시판입니다.'), 409
+            return jsonify(message='이미 등록된 게시판입니다.'), 400
 
         board = Board(name=name)
         board.save()
 
         return '', 200
+
+
+    # 게시판 목록 조회
+    @route('/', methods=['GET'])
+    def list_board(self):
+        category = request.args['category']
+        page = request.args.get('page',1,int)
+
+        # pagination
+        limit = 10
+        skip = (page-1)*limit
+
+        if Board.objects(name=category, is_deleted=False):
+            post_list = Board.objects(name=category, is_deleted=False).get().post
+            post_data = [
+                {"total": len(post_list),
+                 "posts": [{"post_id": post.post_id,
+                            "title": post.title,
+                            "content": post.content,
+                            "created_at": post.created_at,
+                            "likes": len(post.likes)} for post in post_list[skip:skip + limit]]
+                 }]
+            return jsonify(data=post_data), 200
+        return jsonify(message='없는 게시판입니다.'), 400
 
 
     # 게시글 작성 API
@@ -64,7 +86,3 @@ class BoardView(FlaskView):
 
         post = Board.objects(name=board_name).get().post[post_id-1]
         return jsonify(post.to_json()), 200
-
-
-
-
