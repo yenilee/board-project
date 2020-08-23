2206ea5f8039b3b294ae58236b8c0ca99fb3d202
2cong <cjina0418@gmail.com>
2020-08-23 12:42:11 +0900

    Add : 게시판 추가 api 생성

--- app/views/board.py
+++ app/views/board.py
@@ -16,3 +16,18 @@ class BoardView(FlaskView):
         for board in board_data]
 
         return jsonify(data=board_menu), 200
+
+    # 게시판 생성
+    @route('', methods=['POST'])
+    def post(self):
+
+        name = json.loads(request.data)['name']
+
+        # 현재 존재하는 board와 이름 중복 인
+        if Board.objects(name=name, is_deleted=False):
+            return jsonify(message='이미 등록된 게시판입니다.'), 409
+
+        board = Board(name=name)
+        board.save()
+
+        return '', 200
