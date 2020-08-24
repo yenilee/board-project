070b5d72a54adc2a8bf397f5792cbf0940ad053a
2cong <cjina0418@gmail.com>
2020-08-24 15:49:42 +0900

    Update : 게시판 목록조회 수정

--- app/views/board.py
+++ app/views/board.py
@@ -1,20 +1,20 @@
 import json
 
-from app.models         import Board, Post, User
-from flask_classful     import FlaskView, route
-from flask              import jsonify, request, g
-from app.utils          import auth
+from app.models import Board, Post, User
+from flask_classful import FlaskView, route
+from flask import jsonify, request, g
+from app.utils import auth
 
 
 class BoardView(FlaskView):
     # 게시판 카테고리
     @route('/category', methods=['GET'])
     def get_board_category(self):
-        board_data = Board.objects(is_deleted = False)
+        board_data = Board.objects(is_deleted=False)
 
         board_category = [
             {"name": board.name}
-        for board in board_data]
+            for board in board_data]
 
         return jsonify(data=board_category), 200
 
@@ -27,7 +27,7 @@ class BoardView(FlaskView):
 
         # 유저의 권한 확인
         if User.objects(id=g.user).get().master_role == False:
-            return jsonify(message='권한이 없습니다.'),403
+            return jsonify(message='권한이 없습니다.'), 403
 
         # 현재 존재하는 board와 이름 중복 확인
         if Board.objects(name=name, is_deleted=False):
@@ -43,22 +43,25 @@ class BoardView(FlaskView):
     @route('/', methods=['GET'])
     def list_board(self):
         category = request.args['category']
-        page = request.args.get('page',1,int)
+        page = request.args.get('page', 1, int)
 
         # pagination
         limit = 10
-        skip = (page-1)*limit
+        skip = (page - 1) * limit
 
         if Board.objects(name=category, is_deleted=False):
             post_list = Board.objects(name=category, is_deleted=False).get().post
+
             post_data = [
                 {"total": len(post_list),
-                 "posts": [{"post_id": post.post_id,
+                 "posts": [{"number": n,
+                            "post_id": post.post_id,
                             "title": post.title,
                             "content": post.content,
                             "created_at": post.created_at,
-                            "likes": len(post.likes)} for post in post_list[skip:skip + limit]]
-                 }]
+                            "likes": len(post.likes),
+                            "is_deleted":post.is_deleted}
+                           for n, post in zip(range(len(post_list) - skip, 0, -1), post_list[skip:skip + limit])]}]
             return jsonify(data=post_data), 200
         return jsonify(message='없는 게시판입니다.'), 400
 
@@ -71,10 +74,10 @@ class BoardView(FlaskView):
 
         board = Board.objects(name=board_name).get()
         post = Post(
-            author     = g.user,
-            title      = data['title'],
-            content    = data['content'],
-            post_id    = len(board.post)+1
+            author=g.user,
+            title=data['title'],
+            content=data['content'],
+            post_id=len(board.post) + 1
         )
         board.post.append(post)
         board.save()
