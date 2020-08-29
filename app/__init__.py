4d6bd336f46d32f5073a30630d323d53ce925627
yeeunlee <chloe.yeeunl@gmail.com>
2020-08-30 02:19:22 +0900

    add: pymongo 데이터베이스 연결 에러 추가

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,15 @@ import mongoengine
 from flask      import Flask
 from app.config import MONGO_URI
 
+
 def create_app():
     app = Flask(__name__)
     app.debug = True
 
-    mongoengine.connect(host=MONGO_URI)
+    try:
+        mongoengine.connect(host=MONGO_URI)
+    except Exception as e:
+        print('Warning! 데이터베이스 에러 - ' + str(e))
 
     from flask_cors import CORS
     CORS(app, resources={r'*':{'origins':'*'}})
