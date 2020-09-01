0d933d4554a17478bd90f4a7076a4e981e36fbfd
yeeunlee <chloe.yeeunl@gmail.com>
2020-09-01 13:53:21 +0900

    add: factory 파일 추가, conftest 작성

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,19 @@
-import mongoengine
-from app.config import TestConfig
+import pytest
 
-mongoengine.connect(host=TestConfig.MONGO_URI)
-mongoengine.disconnect()
+from flask import current_app
+from app.config import TestConfig
+from unittest import mock
+
+
+@pytest.fixture(scope="session")
+def app():
+    from app import create_app
+    app = create_app()
+    return app
+
+
+@pytest.fixture(scope='function', autouse=True)
+def db(app):
+    import mongoengine
+    mongoengine.connect(host=current_app.config['MONGO_URI'])
+    mongoengine.disconnect()
