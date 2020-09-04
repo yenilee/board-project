3f980b160fc4b0ee0c30117859a10ce2a131be42
yeeunlee <chloe.yeeunl@gmail.com>
2020-09-04 17:36:25 +0900

    fix: app seralizers 관련 디렉토리 정리

--- app/serializers/comment.py
+++ app/serializers/comment.py
@@ -1,5 +1,15 @@
 from marshmallow import fields, Schema
+from .user import UserSchema
 
 
 class CommentCreateSchema(Schema):
     content = fields.Str(required=True)
+
+class CommentSchema(Schema):
+    id = fields.Str(required=True)
+    content = fields.Str(required=True)
+    author = fields.Nested(UserSchema, dump_only=("id", "account"))
+    created_at = fields.DateTime(required=True)
+    like_count = fields.Integer(required=True)
+    #like_count = fields.Method('calculate_like_count')
+    is_replied = fields.Bool(required=True)
--- app/serializers/user.py
+++ app/serializers/user.py
@@ -1,6 +1,9 @@
 from marshmallow import fields, Schema
 
+
 class UserSchema(Schema):
+    id = fields.Str(required=True, uniqe=True)
     account = fields.Str(required=True, unique=True)
-    password = fields.Str(required=True)
-    created_at = fields.DateTime()
+    password = fields.Str(required=True, load_only=True)
+    created_at = fields.DateTime(required=True, load_only=True)
+
