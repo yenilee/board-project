84bf509e83d33f4ff263660d42e52ce7e269fdf9
yeeunlee <chloe.yeeunl@gmail.com>
2020-09-07 12:03:51 +0900

    config 추가

---
+++ app/config.py
@@ -0,0 +1,20 @@
+
+
+class Config:
+    SECRET = 'hiitssecret'
+    ALGORITHM = 'HS256'
+    TESTING = False
+    MONGO_URI = 'mongodb://localhost:27017/board_test'
+
+class LocalConfig(Config):
+    MONGO_URI = 'mongodb://localhost:27017/board_test'
+    TESTING = True
+    DEBUG = True
+
+class TestConfig(Config):
+    MONGO_URI = 'mongomock://127.0.0.1:27017/board_test?connect=false'
+    TESTING = True
+
+
+
+
