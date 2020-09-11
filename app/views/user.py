1b1d39ae6108f25afd2f213fa55422ff2eb88ac8
yeeunlee <chloe.yeeunl@gmail.com>
2020-09-11 15:59:58 +0900

    fix: 로그인 로직 수정 & 스키마, 모델 method 추가

--- app/views/user.py
+++ app/views/user.py
@@ -7,10 +7,10 @@ from flask import jsonify, request, g, current_app
 from bson.json_util import dumps
 
 from app.models import User, Post, Comment
-from app.utils import login_required, user_validator, pagination, user_create_validator
-from app.serializers.user import UserCreateSchema
-from app.serializers.post import PostListSchema, PaginatedPostsSchema
-from app.serializers.comment import CommentListSchema
+from app.utils import login_required, user_validator, user_create_validator
+from app.serializers.user import UserCreateSchema, UserSchema
+from app.serializers.post import PaginatedPostsSchema, PaginatedPostsInBoardSchema
+from app.serializers.comment import PaginatedCommentsSchema
 
 
 class UserView(FlaskView):
@@ -39,45 +39,51 @@ class UserView(FlaskView):
         작성자: avery
         :return: 로그인 인증 토큰
         """
-        data = json.loads(request.data)
+        login_request = json.loads(request.data)
+        user = UserSchema().load(login_request)
 
-        if not User.objects(account=data['account']):
-            return jsonify(message='존재하지 않는 ID입니다.'), 401
+        if not user:
+            return {'message' : '존재하지 않는 사용자입니다.'}, 401
 
-        user = User.objects(account=data['account']).get()
+        if not user.check_password(login_request['password']):
+            return {'message': '잘못된 비밀번호 입니다.'}, 401
 
-        if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
-            token = jwt.encode({"user_id" : dumps(user.id),
-                                "is_master" : user.master_role}, current_app.config['SECRET'], current_app.config['ALGORITHM'])
-            return jsonify(token.decode('utf-8')), 200
+        token = jwt.encode({"user_id" : dumps(user.id), "is_master" : user.master_role},
+                           current_app.config['SECRET'], current_app.config['ALGORITHM'])
 
-        return jsonify(message='잘못된 비밀번호 입니다.'), 401
+        return jsonify(token.decode('utf-8')), 200
 
 
     @route('/posts', methods=['GET'])
     @login_required
-    def my_post(self):
+    def my_post(self, page=1):
         """
         마이페이지: 사용자 작성 게시물 조회 API
         작성자: avery
         :return: 최신 게시물 10개
         """
-        posts = Post.objects(author=g.user_id, is_deleted=False).order_by('-created_at').limit(20)
-        my_page = PostListSchema(many=True).dump(posts)
-        return jsonify(my_posts=my_page), 200
+        if request.args:
+            page = int(request.args.get('page'))
+
+        posts = Post.objects(author=g.user_id, is_deleted=False).order_by('-created_at').paginate(page=page, per_page=10)
+        my_posts = PaginatedPostsInBoardSchema().dump(posts)
+        return jsonify(my_posts), 200
 
 
     @route('/comments', methods=['GET'])
     @login_required
-    def my_comment(self):
+    def my_comment(self, page=1):
         """
         마이페이지: 사용자 작성 댓글 조회 API
         작성자: avery
         :return: 최신 댓글 10개
         """
-        comments = Comment.objects(author=g.user_id, is_deleted=False, is_reply=False)
-        my_comments = CommentListSchema(many=True).dump(comments)
-        return jsonify(my_comments=my_comments), 200
+        if request.args:
+            page = int(request.args.get('page'))
+
+        comments = Comment.objects(author=g.user_id, is_deleted=False, is_reply=False).order_by('-created_at').paginate(page=page, per_page=10)
+        my_comments = PaginatedCommentsSchema().dump(comments)
+        return jsonify(my_comments  ), 200
 
 
     @route('/liked-posts', methods=['GET'])
